62a2ba2b17b83cb0141d2d5960623c22c473cf2c
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-08-08 22:22:02 +0200

    updated the index file

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 let choices = ["Rock", "Paper", "Scissors"];
-let rockBtn = document.getElementById("rock");
-let paperBtn = document.getElementById("paper");
-let scissorBtn = document.getElementById("scissor");
 let computerChoice = document.getElementById("computerChoice");
 let userChoice = document.getElementById("userChoice");
 let winnerElement = document.getElementById("winner");
 //functions
-rockBtn.addEventListener("click", gamePlay);
-function gamePlay() {
-  let randomIndex = Math.floor(Math.random() * 3);
-  computerChoice.innerText = choices[randomIndex];
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let randomIndex = Math.floor(Math.random() * 3);
+    computerChoice.innerText = choices[randomIndex];
+  });
+});
